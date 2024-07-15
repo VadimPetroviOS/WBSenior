a72fd4c47c105a2f7a611c4ccb4bf06fce3596d9
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-07-15 08:03:12 +0300

    first commit

---
+++ WBSenior/WBSeniorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSeniorApp.swift
+//  WBSenior
+//
+//  Created by Вадим on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSeniorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
